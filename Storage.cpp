44111225395143e84832c90c886b315d664ce82d
BartekSA <34489058+BartekSA@users.noreply.github.com>
2018-01-10 09:17:50 +0100

    Add files via upload

--- Storage.cpp
+++ Storage.cpp
@@ -1,33 +1,28 @@
 #include "Storage.hpp"
 
-Storage::Storage(double nId){
+Storage::Storage(double nId) {
 	id = nId;
 
-			store= std::vector<Product> s;
-			//czy trzeba deklarowaæ wektory i tablice?
+	store = std::vector < Product > s;
+	//czy trzeba deklarowaæ wektory i tablice?
 }
-void Storage::setId(double newID){
+void Storage::setId(double newID) {
 	id = newID;
 }
-void Storage::showStore()
-	{
-		for (int i : store)
-		{
-			store[i].show();
-		}
+void Storage::showStore() {
+	for (int i : store) {
+		store[i].show();
 	}
+}
 
-int Storage::getId()
-	{
-		return id;
-	}
+int Storage::getId() {
+	return id;
+}
 
-nodeType Storage::getType()
-	{
-		return Storage;
-	}
-int Storage::recieve(Product recieved)
-	{
-		store.push_back(recieved);
-		return 1;
-	}
+nodeType Storage::getType() {
+	return nodeType::STORAGE;
+}
+int Storage::recieve(Product recieved) {
+	store.push_back(recieved);
+	return 1;
+}
